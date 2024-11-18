900dbb283a2b5886801af828403e393fc0b789e2
Cesar Santos <csar.santos18@gmail.com>
2024-11-18 15:38:17 -0500

    Initial commit for cantinaOnline backend

---
+++ src/main/java/com/cantinaOnline/cantinaOnline/CantinaOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.cantinaOnline.cantinaOnline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CantinaOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CantinaOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cantinaOnline/cantinaOnline/CantinaOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cantinaOnline.cantinaOnline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CantinaOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
